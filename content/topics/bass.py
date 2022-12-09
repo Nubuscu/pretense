5a8403cdf754785fa862dc9f328fa315a2cb2dc4
Tyler Kennedy <tyler.kennedy@seequent.com>
2022-12-09 16:15:25 +1300

    Add basics of old content
    not fully detailed, just a few lists of albums. Probably worth
    re-writing the actual body with a clearer head.

---
+++ content/topics/bass.py
@@ -0,0 +1,21 @@
+ALBUMS = [
+        "Antipop",
+    "Rage Against The Machine",
+    "The Battle Of Los Angeles",
+    "Absolution",
+    "10,000 Days",
+    "Lateralus",
+    "The Physical World",
+    "Royal Blood",
+]
+
+
+# topic name (what shows in cytoscape)
+TOPIC = "Bass"
+# review name (could be longer/descriptive)
+TITLE = "Learning bass"
+
+# text content. Formatted as markdown in the frontend.
+BODY = """
+
+"""
---
+++ content/topics/screaming.py
@@ -0,0 +1,20 @@
+ALBUMS = [
+    "III",
+    "Awakened",
+    "Wasting Light",
+    "The Triptych",
+    "Constellations",
+    "thegodmachine",
+    "Slave to Nothing",
+]
+
+
+# topic name (what shows in cytoscape)
+TOPIC = "Screaming"
+# review name (could be longer/descriptive)
+TITLE = "Harsh vocals as not just noise"
+
+# text content. Formatted as markdown in the frontend.
+BODY = """
+
+"""
